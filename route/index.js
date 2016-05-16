25c3e223989bd5423a9179eb35e705b8019a8ea1
James Gu <james_gu14@hotmail.com>
2016-05-16 16:06:30 +0800

    Implemented logger function

--- route/index.js
+++ route/index.js
@@ -1,12 +1,14 @@
 'use strict'
 
 var basePath = '/';
+var log = require('senso-logger');
 
 module.exports.routers = [
 {
     method: 'GET',
     path: basePath,
     handler: function (request, reply) {
+    	log('Today is a happy day');
         reply.view('./index/home', {
         	title: 'Home Page',
         	message: 'Index - Home Page'
